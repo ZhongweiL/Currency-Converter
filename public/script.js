311ba8224403f7da8deac4cec5350ee86ba93600
Zhongwei Li <zhongweili95@gmail.com>
2020-09-15 18:51:01 -0400

    init commit

---
+++ public/script.js
@@ -0,0 +1,23 @@
+const convertBtn = document.querySelector(".convert-button");
+const output = document.querySelector("#output-amount");
+
+//use exchangerates api for exchange rates
+const url = "https://api.exchangeratesapi.io/latest";
+convertBtn.addEventListener("click", () => {
+	//read input amount from the client
+	const from = document.querySelector("#input-unit").value;
+	const amount = document.querySelector("#input-amount").value;
+	const to = document.querySelector("#output-unit").value;
+	fetch(url + `?base=${from}`)
+		//read the data in json
+		.then(res => res.json())
+		.then(data => {
+			output.setAttribute("readonly", "no");
+			output.value = (data.rates[to] * amount).toFixed(2);
+			output.setAttribute("readonly", "yes");
+		})
+		//catch potentially erros and send it
+		.catch(e => {
+			alert(e.message);
+		});
+});
---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+//use handlebar as view engine
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../public/templates/views"));
+hbs.registerPartials(path.join(__dirname, "../public/templates/partials"));
+
+//render home page and about page
+app.get("/", (req, res) => {
+	res.render("index", { title: "Currency Converter" });
+});
+
+app.get("/about", (req, res) => {
+	res.render("about", { title: "About" });
+});
+
+//render 404 page when the route is invalid
+app.get("/about/*", (req, res) => {
+	res.status(404).render("404", { title: "404", message: "Page not found!" });
+});
+
+app.get("/*", (req, res) => {
+	res.status(404).render("404", { title: "404", message: "Page not found!" });
+});
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+});
